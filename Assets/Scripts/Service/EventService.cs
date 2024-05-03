aa6c3d32bf2fd24adb785aa242827c6d03730569
jervitha <147043738+jervitha@users.noreply.github.com>
2024-05-04 04:13:45 +0530

    added event service

---
+++ Assets/Scripts/Service/EventService.cs
@@ -0,0 +1,31 @@
+
+
+public class EventService
+{
+    private static EventService instance;
+    public static EventService Instance
+    {
+    get
+        {
+        if(instance==null)
+        {
+           instance=new EventService();
+         }
+            return instance;
+        }
+
+
+ 
+     }
+
+    public EventsController OnLightSwitchToggled { get; private set; }
+    public EventService()
+    {
+        OnLightSwitchToggled = new EventsController();
+
+    }
+}
+
+
+
+
